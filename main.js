57a863ab027bc8f7dc32b722d9f20865caf8c609
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-10-06 18:19:46 -0400

    commit!

--- main.js
+++ main.js
@@ -8,3 +8,9 @@
 
 // Feel free to delete this line when starting the task
 console.log("Hello world!")
+console.log("\n\n")
+console.log("Apple Inc.\nOne Apple Park Way\nCupertino, CA\n95014\n+1 408-996-1010")
+console.log("\n\n")
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmon, WA\n98052\n+1 425-882-8080")
+console.log("\n\n")
+console.log("x\to\tx\no\tx\to\nx\to\tx")
